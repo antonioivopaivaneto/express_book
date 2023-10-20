62c274864c13115af30d8d5745013f51a48e9fee
junior <antonioivo.3@gmail.com>
2023-10-20 20:36:27 -0300

    install puppeteer for test integration

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { engine: expressHandlebars } = require('express-handlebars')
-const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 
 const app = express();
 app.engine('handlebars',expressHandlebars({
@@ -14,28 +14,23 @@ app.use(express.static(__dirname + '/public'))
 const port = process.env.PORT || 3001;
 
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.get('/', handlers.home);
+
+app.get('/about', handlers.about);
+
+app.use(handlers.notFound);
+
+app.use(handlers.serverError);
+
+
+if(require.main === module){
+    app.listen(port, () => 
+         console.log('Listening on port', port)
+    );
+
+}else{
+    module.exports = app
+}
 
 
 
-app.get('/about', (req, res) => {
-    res.render('about',{fortune:fortune.getFortune()});
-});
-
-// Página 404 personalizada
-app.use((req, res, next) => {
-    res.status(404);
-    res.render('404');
-});
-
-// Página 500 personalizada
-app.use((err, req, res, next) => {
-    console.log(err.message);
-    res.status(500);
-    res.render('500');
-});
-
-
-app.listen(port, () => console.log('Listening on port', port));
